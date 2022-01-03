b301a419f5f9ad47882f2faeb80f13d834a5af62
vo0doo <kirsanov.bvt@gmail.com>
2022-01-03 22:02:53 +0300

    added color palette item
    Signed-off-by: vo0doo <kirsanov.bvt@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import "./index.scss"
+import "./utils/style/_vars.scss";
+import "./utils/style/_main.scss";
+import "./assets/fonts/fonts.scss";
+import "./components/palette/palette.scss"
+import "./index.scss";
