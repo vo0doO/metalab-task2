a1e688e2f6e9bed4998be1276a808e4b85cf393a
vo0doo <kirsanov.bvt@gmail.com>
2022-01-04 20:46:51 +0300

    fixed punctuation error
    _________________________________________
    
    added postcss-flexbugs-fixes plugin
    
    Signed-off-by: vo0doo <kirsanov.bvt@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyPlugin = require("copy-webpack-plugin")
+const presets = require("postcss-preset-env")
+
 
 
 const PATHS = {
