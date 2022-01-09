3f549b3acc89dcd869af79b04668ae669e22404d
vo0doo <kirsanov.bvt@gmail.com>
2022-01-09 06:01:58 +0300

    renamed components typ and pallete and их элементы и модификаторы по соглашению BEM
    Signed-off-by: vo0doo <kirsanov.bvt@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import './_reset.scss';
 import './assets/fonts/fonts.scss';
-import './components/palette/palette.scss';
+import './components/color-palette/color-palette.scss';
 import './components/typography/typography.scss';
 import './index.scss';
 import './utils/style/_main.scss';
