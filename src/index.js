b51b94267e0125f30aa2756372bd50ca7a4b32a7
vo0doo <kirsanov.bvt@gmail.com>
2022-01-16 03:06:44 +0300

    for refactoring "'" in """ updating's montserrat-bold,.svg, montserrat-regular.svg, head.pug, text.js, _main.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './assets/fonts/fonts.scss';
-import './components/color-palette/color-palette.scss';
-import './components/typography/typography.scss';
-import './components/text/text.scss';
-import './index.scss';
-import './utils/style/_main.scss';
-import './utils/style/_vars.scss';
+import "./assets/fonts/fonts.scss";
+import "./components/color-palette/color-palette.scss";
+import "./components/typography/typography.scss";
+import "./components/text/text.scss";
+import "./index.scss";
+import "./utils/style/_main.scss";
+import "./utils/style/_vars.scss";
