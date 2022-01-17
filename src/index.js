d62a4a1345e4b103982a030aadb344fa527e272a
vo0doo <kirsanov.bvt@gmail.com>
2022-01-18 01:50:39 +0300

    обновлен text-field.pug добавлен функционал masked -text-field

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './assets/fonts/fonts.scss';
-import './components/color-palette/color-palette.scss';
-import './components/text-field/text-field.scss';
-import './components/text/text.scss';
-import './components/typography/typography.scss';
-import './index.scss';
-import './utils/style/_main.scss';
-import './utils/style/_vars.scss';
+import "./assets/fonts/fonts.scss";
+import "./components/color-palette/color-palette.scss";
+import "./components/text-field/text-field.js";
+import "./components/text-field/text-field.scss";
+import "./components/text/text.scss";
+import "./components/typography/typography.scss";
+import "./index.scss";
+import "./utils/style/_main.scss";
+import "./utils/style/_vars.scss";
