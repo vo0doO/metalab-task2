215bea8db7d6daca1694a11a3073f5f297e86061
vo0doo <kirsanov.bvt@gmail.com>
2022-01-17 22:05:11 +0300

    обновлены index.scss index.pug index.js - добавленны text-field и text-field-focused

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import "./assets/fonts/fonts.scss";
-import "./components/color-palette/color-palette.scss";
-import "./components/typography/typography.scss";
-import "./components/text/text.scss";
-import "./index.scss";
-import "./utils/style/_main.scss";
-import "./utils/style/_vars.scss";
+import './assets/fonts/fonts.scss';
+import './components/color-palette/color-palette.scss';
+import './components/text-field/text-field.scss';
+import './components/text/text.scss';
+import './components/typography/typography.scss';
+import './index.scss';
+import './utils/style/_main.scss';
+import './utils/style/_vars.scss';
