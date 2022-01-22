d69f065f79b46b494dd52e9869f99578bf79ad64
vo0doo <kirsanov.bvt@gmail.com>
2022-01-23 00:38:41 +0300

    Update package.json package-lock.json for install new PluginPug and pug-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,131 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const PugPlugin = require('pug-plugin');
 
 const PATHS = {
-  src: path.resolve(__dirname, "src"),
-  dist: path.resolve(__dirname, "dist"),
+  src: path.resolve(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'dist'),
 };
 
-let mode = "development";
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
+// Default pug-loader options.
+const pugLoaderOptions = {
+  method: 'compile',
+  esModule: true,
+};
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
 }
-console.log(mode + " mode");
+console.log(`${mode} mode`);
 
 module.exports = {
-  mode: mode,
-  devtool: "eval-cheap-module-source-map",
+  mode,
+  resolve: {
+    alias: {
+      Components: path.join(PATHS.src, '/components/'),
+      Images: path.join(PATHS.src, '/assets/img/'),
+      Templates: path.join(PATHS.src, '/templates/'),
+      'inputmask.dependencyLib': path.join(
+        __dirname,
+        'node_modules/jquery.inputmask/extra/dependencyLibs/inputmask.dependencyLib.js',
+      ),
+      inputmask: path.join(
+        __dirname,
+        'node_modules/jquery.inputmask/dist/inputmask/inputmask.js',
+      ),
+    },
+  },
+  resolveLoader: {
+    // alias for pug-loader
+    alias: {
+      'pug-loader2': '@webdiscus/pug-loader',
+    },
+  },
+
+  devtool: 'eval-cheap-module-source-map',
   externals: {
     paths: PATHS,
   },
   entry: {
-    scripts: "./src/index.js",
+    scripts: './src/index.js',
+    // index: './src/index.pug',
+
   },
 
   output: {
     path: PATHS.dist,
-    filename: "js/[name].[contenthash].js",
+    publicPath: '/',
+    filename: 'js/[name].[contenthash].js',
     clean: true,
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   module: {
     rules: [
       {
         test: /\.pug$/,
-        loader: "pug-loader",
+        loader: 'pug-loader',
+        options: pugLoaderOptions,
       },
       {
-        test: /\.pug$/,
-        loader: "html-loader",
+        test: /\.html$/,
+        loader: 'html-loader',
       },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
 
       {
         test: /\.(png|svg|jpg|jpeg|gif|bmp)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/img/[name][ext][query]",
+          filename: 'assets/img/[name][ext][query]',
         },
       },
       {
         test: /\.(woff|svg|woff2|oet|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/fonts/[name][ext][query]",
+          filename: 'assets/fonts/[name][ext][query]',
         },
       },
       {
         test: /\.scss$/,
         use: [
-          "production" === process.env.NODE_ENV
-              ? MiniCssExtractPlugin.loader
-              : "style-loader",
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
           {
-            loader: "css-loader",
-            options: {sourceMap: true},
+            loader: 'css-loader',
+            options: { sourceMap: true },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               sourceMap: true,
               postcssOptions: {
                 plugins: [
-                  require("autoprefixer"),
-                  require("postcss-flexbugs-fixes"),
-                  require("postcss-combine-media-query"),
-                  require("css-mqpacker"),
-                  require("cssnano")({
+                  require('autoprefixer'),
+                  require('postcss-flexbugs-fixes'),
+                  require('postcss-combine-media-query'),
+                  require('css-mqpacker'),
+                  require('cssnano')({
                     preset: [
-                      "default",
+                      'default',
                       {
                         discardComments: {
                           removeAll: true,
@@ -105,31 +138,31 @@ module.exports = {
             },
           },
           {
-            loader: "sass-loader",
-            options: {sourceMap: true},
+            loader: 'sass-loader',
+            options: { sourceMap: true },
           },
         ],
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
-            options: {sourceMap: true},
+            loader: 'css-loader',
+            options: { sourceMap: true },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               sourceMap: true,
               postcssOptions: {
                 plugins: [
-                  require("autoprefixer"),
-                  require("postcss-combine-media-query"),
-                  require("css-mqpacker"),
-                  require("cssnano")({
+                  require('autoprefixer'),
+                  require('postcss-combine-media-query'),
+                  require('css-mqpacker'),
+                  require('cssnano')({
                     preset: [
-                      "default",
+                      'default',
                       {
                         discardComments: {
                           removeAll: true,
@@ -146,36 +179,29 @@ module.exports = {
     ],
   },
   plugins: [
+    // new PugPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/assets/favicons"),
-          to: "./assets/favicons",
+          from: path.resolve(__dirname, 'src/assets/favicons'),
+          to: './assets/favicons',
         },
         {
-          from: path.resolve(__dirname, "src/assets/img"),
-          to: "./assets/img",
+          from: path.resolve(__dirname, 'src/assets/img'),
+          to: './assets/img',
         },
         {
-          from: path.resolve(__dirname, "src/utils/js/worker.js"),
-          to: "./utils/js/worker.js",
+          from: path.resolve(__dirname, 'src/utils/js/worker.js'),
+          to: './utils/js/worker.js',
         },
       ],
     }),
     new HtmlWebpackPlugin({
-      template: "!!pug-loader!/src/index.pug",
+      template: '!!pug-loader!./src/index.pug',
     }),
 
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
+      filename: '[name].[contenthash].css',
     }),
   ],
-  resolve: {
-    alias: {
-      "inputmask.dependencyLib": path.join(__dirname,
-          "node_modules/jquery.inputmask/extra/dependencyLibs/inputmask.dependencyLib.js"),
-      "inputmask": path.join(__dirname,
-          "node_modules/jquery.inputmask/dist/inputmask/inputmask.js"),
-    },
-  },
 };
