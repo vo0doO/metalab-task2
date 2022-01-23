2b79263cf6f45a83905d39f8b8a963d8067d8cfd
vo0doo <kirsanov.bvt@gmail.com>
2022-01-23 22:04:32 +0300

    new Input

--- src/components/text-field/text-field.js
+++ src/components/text-field/text-field.js
@@ -1,21 +1,14 @@
-// Add extensions as necessary make sure you remember to add the corresponding
-// aliases in the webpack config
-
-import $ from 'jquery'
-import 'jquery.inputmask'
-
+import $ from 'jquery';
+import 'jquery.inputmask';
 
 window.onload = (event) => {
-	$(() => {
-		let input = $('.text-field__input.text-field__input_with-mask')
-		console.log(input)
-		input.inputmask('dd.mm.yyyy', {
-			alias: 'date',
-			placeholder: 'ДД.ММ.ГГГГ',
-			showMaskOnFocus: false,
-			showMaskOnHover: false
-			
-		})
-	})
+  $(() => {
+    const input = $('.input__text.input__text_with-mask');
+    input.inputmask('dd.mm.yyyy', {
+      alias: 'date',
+      placeholder: 'ДД.ММ.ГГГГ',
+      showMaskOnFocus: false,
+      showMaskOnHover: false,
+    });
+  });
 };
-
