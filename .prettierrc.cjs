c9529b3dd7a6f209cb676d5eca38607eeafbfbe4
vo0doo <kirsanov.bvt@gmail.com>
2022-01-23 20:39:52 +0300

    Update .prettierrc.cjs added pugSingleQuotes

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,21 +2,22 @@
 /// <reference types="@prettier/plugin-pug/src/prettier" />
 
 /**
- * @type {import('prettier').Options}
- */
+	* @type {import('prettier').Options}
+	*/
 module.exports = {
-	plugins: ['prettier-plugin-pug'],
-	printWidth: 120,
-	useTabs: true,
-	pugTabWidth: 2,
-	singleQuote: true,
-	pugAttributeSeparator: 'none',
-	pugSortAttributes: 'desc',
-	pugWrapAttributesThreshold: 4,
-	pugEmptyAttributes: 'all',
-	pugArrowParens: 'always',
-	pugClassNotation: 'literal',
-	pugIdNotation: 'literal',
-	pugClassLocation: 'before-attributes',
-	pugExplicitDiv: false
+		plugins: [ 'prettier-plugin-pug' ],
+		printWidth: 120,
+		useTabs: true,
+		pugTabWidth: 2,
+		singleQuote: true,
+		pugSingleQuote: 'true',
+		pugAttributeSeparator: 'none',
+		pugSortAttributes: 'desc',
+		pugWrapAttributesThreshold: 4,
+		pugEmptyAttributes: 'all',
+		pugArrowParens: 'always',
+		pugClassNotation: 'literal',
+		pugIdNotation: 'literal',
+		pugClassLocation: 'before-attributes',
+		pugExplicitDiv: false
 }
