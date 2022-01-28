71780f84704eda6831619448afa1b72529ae10fb
vo0doo <kirsanov.bvt@gmail.com>
2022-01-28 16:24:22 +0300

    Created counter.js - for added functional for button

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,1 +1,30 @@
-import './counter.scss';
+import $ from 'jquery';
+import { log, setLogLevel } from 'webpack-dev-server/client/utils/log';
+
+setLogLevel('verbose');
+
+const initCounter = async (args) => {
+  if (typeof args === 'undefined') {
+    throw new Error('Undefined arguments');
+  }
+  try {
+    const increment = $('.counter__input');
+    log('Increment', increment);
+  } catch (e) {
+    log('При инициализации кнопок коунтера возникла ошибка: ', e);
+    throw new Error(e);
+  }
+};
+
+const ready = async () => {
+  try {
+    document.addEventListener('DOMContentLoaded', (e) => {
+      log('DOM loaded: ', e);
+    });
+    return true;
+  } catch (e) {
+    throw new Error(`DOM not loaded: ${e}`);
+  }
+};
+
+initCounter(ready()).then((r) => { log(r); });
