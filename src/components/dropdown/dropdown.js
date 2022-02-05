67b347e0ed86ed45a3a479756e59f9719d55eba2
vo0doo <kirsanov.bvt@gmail.com>
2022-02-05 20:54:10 +0300

    Updated dropdown.js - full refactoring

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,44 +1,96 @@
-async function initDropDown() {
+import $, {
+  event
+} from 'jquery';
 
-  try {
-    const {
-      default: jQuery
-    } = await import('jquery');
-    const $ = jQuery.noConflict();
-    const dropdown = $('.dropdown');
-    const arrowBtn = dropdown.find('.dropdown__button');
-    const dropItems = dropdown.find('.dropdown__dropitems');
-    const box = dropdown.find('.dropdown__input');
-    const dropdownShowHide = (event) => {
-      event.preventDefault();
-      dropItems.toggleClass('dropdown__dropitems_closed');
-      arrowBtn.toggleClass('dropdown__button_opened');
-      box.toggleClass('dropdown__input_opened');
+
+class DropDown {
+
+  constructor() {
+    this.root = $('.dropdown');
+    this.arrowBtn = $('.dropdown__arrow-button');
+    this.items = $('.dropdown__items');
+    this.input = $('.dropdown__input');
+    this.showHideItems = () => {
+      this.items.trigger('dropdown:toggle');
+      this.input.trigger('dropdown:toggle');
+      this.arrowBtn.trigger('dropdown:toggle');
     };
+  }
 
-    [
-      box,
-      arrowBtn,
-    ].map((item) => item.on('click', dropdownShowHide));
-    return 0;
-  } catch (error) {
-    throw new Error(`${error}`);
+  get showHide() {
+    return this.showHideItems;
+  }
+
+  ariaExpandedToggle(elem) {
+    if (elem.attr('aria-expanded') === 'false') {
+      elem.attr('aria-expanded', 'true');
+    } else if (elem.attr('aria-expanded') === 'true') {
+      elem.attr('aria-expanded', 'false');
+    }
+  }
+
+  itemsEvents() {
+    this.items.on({
+      'dropdown:toggle': function (event) {
+        var elem = $(this);
+        elem.toggleClass('dropdown__items_closed');
+      }
+    });
+  }
+
+  inputEvents() {
+    const ctx = this;
+    ctx.input.on({
+      'click': ctx.showHide,
+      'dropdown:toggle': function (event) {
+        var elem = $(this);
+        elem.toggleClass('dropdown__input_opened');
+        ctx.ariaExpandedToggle(elem);
+      },
+      'keyup': function (event) {
+        if (event.which === 32) {
+          ctx.items.trigger('dropdown:toggle');
+          ctx.input.trigger('dropdown:toggle');
+          ctx.arrowBtn.trigger('dropdown:toggle');
+        }
+      }
+    });
+  }
+
+  arrowBtnEvents() {
+    this.arrowBtn.on({
+      'click': this.showHide,
+      'dropdown:toggle': function (event) {
+        var elem = $(this);
+        elem.toggleClass('dropdown__arrow-button_opened');
+      }
+    });
+  }
+
+  init() {
+    console.log("DropDown init");
+    this.itemsEvents();
+    this.inputEvents();
+    this.arrowBtnEvents();
   }
 }
+
 const ready = async () => {
   try {
     document.addEventListener('DOMContentLoaded', () => {
       console.info('DOM loaded');
+      const dropdown = new DropDown();
+      dropdown.init();
     });
     return true;
   } catch (e) {
     throw new Error(`DOM not loaded: ${e}`);
   }
 };
-ready().then(() => {
-  initDropDown();
-});
+
+
+ready();
 
 export {
-  initDropDown
+  DropDown
 };
