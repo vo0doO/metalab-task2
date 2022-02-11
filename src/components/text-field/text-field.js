0ca765453b56d4386c1c184c3639c0bb29a538ea
vo0doo <kirsanov.bvt@gmail.com>
2022-02-11 17:48:12 +0300

    Updated text-field.js - formattin style

--- src/components/text-field/text-field.js
+++ src/components/text-field/text-field.js
@@ -4,16 +4,16 @@
 // import 'jquery.inputmask';
 
 window.onload = async function () {
-	const { default: jQuery } = await import('jquery')
-	const $ = jQuery.noConflict()
-	const { default: inputmask } = await import('jquery.inputmask')
+	const { default: jQuery } = await import('jquery');
+	const $ = jQuery.noConflict();
+	const { default: inputmask } = await import('jquery.inputmask');
 	$(() => {
-		const input = $('.text-field__input.text-field__input_with-mask')
+		const input = $('.text-field__input.text-field__input_with-mask');
 		input.inputmask('dd.mm.yyyy', {
 			alias: 'date',
 			placeholder: 'ДД.ММ.ГГГГ',
 			showMaskOnFocus: false,
 			showMaskOnHover: false
-		})
-	})
-}
+		});
+	});
+};
