293267b4d52a4e6c080defc61c0c1dce5b309979
vo0doo <kirsanov.bvt@gmail.com>
2022-02-12 05:43:41 +0300

    Updated counter.js - added events list, sorting code lines

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,18 +1,28 @@
 import $ from 'jquery';
 
-export default class CustomCounter extends HTMLElement {
-	static get classList() {
+export default class Counter extends HTMLElement {
+	static get classes() {
 		return {
-			FIELD: 'counter__field',
-			DECREMENT: 'counter__decrement-button',
-			INCREMENT: 'counter__increment-button',
 			INPUT: 'counter__input',
-			HIDDEN: 'counter__input_hidden'
+			FIELD: 'counter__field',
+			HIDDEN: 'counter__input_hidden',
+			INCREMENT: 'counter__increment-button',
+			DECREMENT: 'counter__decrement-button',
+		};
+	}
+
+	static get events() {
+		return {
+			CHANGE_INPUT_VALUE: 'change.counter.input.value',
+			CLICK_INCREMENT_BUTTON: 'click.counter.increment-button',
+			CLICK_DECREMENT_BUTTON: 'click.counter.decrement',
+			CHANGE_INCREMENT_BUTTON_STYLE: 'change.counter.increment-button.style',
+			CHANGE_DECREMENT_BUTTON_STYLE: 'change.counter.decrement-button.style'
 		};
 	}
 
 	static setDisabled(element) {
-		const disabled = CustomCounter.getDisabled(element);
+		const disabled = Counter.getDisabled(element);
 		if (disabled === 'disabled') {
 			return;
 		}
@@ -24,7 +34,7 @@ export default class CustomCounter extends HTMLElement {
 	}
 
 	static removeDisabled(element) {
-		const disabled = CustomCounter.getDisabled(element);
+		const disabled = Counter.getDisabled(element);
 		if (disabled !== 'disabled') {
 			return;
 		}
@@ -35,7 +45,7 @@ export default class CustomCounter extends HTMLElement {
 		return {
 			attributes: true,
 			attributeOldValue: true,
-			attributeFilter: CustomCounter.observedAttributes
+			attributeFilter: Counter.observedAttributes
 		};
 	}
 
@@ -47,33 +57,33 @@ export default class CustomCounter extends HTMLElement {
 		super();
 
 		this.counterInput = $(`.counter__input_hidden#${this.id}`);
-		this.counterIncrementButton = $(`.counter__increment-button#${this.id}`);
-		this.counterDecrementButton = $(`.counter__decrement-button#${this.id}`);
 		this.connectedCallback = this.connectedCallback.bind(this);
+		this.counterInputEvents = this.counterInputEvents.bind(this);
 		this.disconnectedCallback = this.disconnectedCallback.bind(this);
-		this.attributeChangedCallback = this.attributeChangedCallback.bind(this);
 		this.counterIncrementEvents = this.counterIncrementEvents.bind(this);
 		this.counterDecrementEvents = this.counterDecrementEvents.bind(this);
-		this.counterInputEvents = this.counterInputEvents.bind(this);
-		this.handleChangeCounterInputValue = this.handleChangeCounterInputValue.bind(this);
+		this.counterIncrementButton = $(`.counter__increment-button#${this.id}`);
+		this.counterDecrementButton = $(`.counter__decrement-button#${this.id}`);
+		this.attributeChangedCallback = this.attributeChangedCallback.bind(this);
 		this.handleClickCounterButton = this.handleClickCounterButton.bind(this);
 		this.counterObserver = new MutationObserver(this.counterObserverCallback);
+		this.handleChangeCounterInputValue = this.handleChangeCounterInputValue.bind(this);
 	}
 
 	connectedCallback() {
+		this.counterObserve();
+		this.counterInputEvents();
 		this.counterDecrementEvents();
 		this.counterIncrementEvents();
-		this.counterInputEvents();
-		this.counterObserve();
 	}
 
 	disconnectedCallback() {
+		this.counterObserver.takeRecords();
 		this.counterInput.off('change.counter.input.value');
+		this.counterDecrementButton.off('click.counter.decrement');
 		this.counterIncrementButton.off('click.counter.increment-button');
 		this.counterIncrementButton.off('change.counter.increment-button.style');
 		this.counterDecrementButton.off('change.counter.decrement-button.style');
-		this.counterDecrementButton.off('click.counter.decrement');
-		this.counterObserver.takeRecords();
 	}
 
 	attributeChangedCallback(element, newValue, oldValue) {
@@ -103,20 +113,24 @@ export default class CustomCounter extends HTMLElement {
 	}
 
 	handleChangeCounterButtonState(_event, data) {
+		const val = parseInt(data.input.val(), 10);
 		const max = parseInt(data.input.attr('max'), 10);
 		const min = parseInt(data.input.attr('min'), 10);
-		const val = parseInt(data.input.val(), 10);
+
 		if (val < max && val >= min) {
-			CustomCounter.removeDisabled(data.incr);
+			Counter.removeDisabled(data.incr);
 		}
+
 		if (val >= max) {
-			CustomCounter.setDisabled(data.incr);
+			Counter.setDisabled(data.incr);
 		}
+
 		if (val > min) {
-			CustomCounter.removeDisabled(data.decr);
+			Counter.removeDisabled(data.decr);
 		}
+
 		if (val <= min) {
-			CustomCounter.setDisabled(data.decr);
+			Counter.setDisabled(data.decr);
 		}
 	}
 
@@ -128,19 +142,22 @@ export default class CustomCounter extends HTMLElement {
 
 	handleClickCounterButton(event) {
 		const element = event.target;
-		const input = element.nextSibling || element.previousSibling;
 		const cl = element.className;
+		const input = element.nextSibling || element.previousSibling;
+
 		switch (cl) {
 			case 'counter__increment-button': {
 				input.stepUp();
 				$(input).attr('value', input.value);
 				break;
 			}
+
 			case 'counter__decrement-button': {
 				input.stepDown();
 				$(input).attr('value', input.value);
 				break;
 			}
+
 			default: {
 				throw new Error(`Ошибка обработки события: ${event}`);
 			}
@@ -149,6 +166,7 @@ export default class CustomCounter extends HTMLElement {
 
 	counterIncrementEvents() {
 		const element = $(this.counterIncrementButton);
+
 		element.on({
 			'click.counter.increment-button': this.handleClickCounterButton,
 			'change.counter.increment-button.style ': this.handleChangeCounterButtonState
@@ -157,6 +175,7 @@ export default class CustomCounter extends HTMLElement {
 
 	counterDecrementEvents() {
 		const element = $(this.counterDecrementButton);
+
 		element.on({
 			'click.counter.decrement': this.handleClickCounterButton,
 			'change.counter.decrement-button.style': this.handleChangeCounterButtonState
@@ -172,8 +191,8 @@ export default class CustomCounter extends HTMLElement {
 	counterObserverCallback(mutations) {
 		mutations.forEach((mutation) => {
 			if (mutation.type === 'attributes') {
-				const { value } = mutation.target;
 				const { oldValue } = mutation;
+				const { value } = mutation.target;
 				if (value !== oldValue) {
 					$(mutation.target).trigger('change.counter.input.value');
 				}
@@ -182,4 +201,4 @@ export default class CustomCounter extends HTMLElement {
 	}
 }
 
-window.customElements.define('guests-counter', CustomCounter);
+window.customElements.define('guests-counter', Counter);
