f31037e1d0d1b6cdc6a09484016b0c611568a218
vo0doo <kirsanov.bvt@gmail.com>
2022-02-18 23:44:59 +0300

    Updated dropdown.** - refactoring from element arrow-buttom to button component

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
-
+import { IconButton } from '../button/button';
+import.meta.webpackHot.accept(
+	'./../../index.js'
+	);
 class DropDown extends HTMLElement {
 	static get classes() {
 		return {
@@ -7,14 +10,8 @@ class DropDown extends HTMLElement {
 			INPUT: 'js-dropdown__input',
 			ITEMS: 'js-dropdown__items',
 			ROOT_OPENED: 'js-dropdown',
-			ARROW_BUTTON: 'js-dropdown__arrow-button',
-			CLEAR_BUTTON: 'js-dropdown__clear-button',
 			INPUT_OPENED: 'js-dropdown__input_opened',
 			ITEMS_OPENED: 'js-dropdown__items_opened',
-			CONFIRM_BUTTON: 'js-dropdown__confirm-button',
-			ARROW_BUTTON_OPENED: 'js-dropdown__arrow-button_opened',
-			CLEAR_BUTTON_OPENED: 'js-dropdown__clear-button_opened',
-			CONFIRM_BUTTON_OPENED: 'js-dropdown__confirm-button_opened',
 		};
 	}
 
@@ -23,18 +20,12 @@ class DropDown extends HTMLElement {
 			CLICK_INPUT: 'click.dropdown.input',
 			CHANGE_ROOT_VALUE: 'change.dropdown.root.value',
 			CHANGE_INPUT_VALUE: 'change.dropdown.input.value',
-			CLICK_ARROW_BUTTON: 'click.dropdown.arrow-button',
-			CLICK_CLEAR_BUTTON: 'click.dropdown.clear-button',
 			TOGGLE_ROOT_OPENED: 'toggle.dropdown.root.opened',
 			TOGGLE_ITEMS_OPENED: 'toggle.dropdown.items.opened',
 			TOGGLE_INPUT_OPENED: 'toggle.dropdown.input.opened',
 			CHANGE_ROOT_DATA_VALUE: 'change.dropdown.root.value',
-			CLICK_CONFIRM_BUTTON: 'click.dropdown.confirm-button',
 			CHANGE_ROOT_PLACEHOLDER: 'change.dropdown.root.placeholder',
 			CHANGE_INPUT_PLACEHOLDER: 'change.dropdown.root.placeholder',
-			TOGGLE_ARROW_BUTTON_OPENED: 'toggle.dropdown.arrowBtn.opened',
-			TOGGLE_CLEAR_BUTTON_OPENED: 'toggle.dropdown.clear-button.opened',
-			TOGGLE_CONFIRM_BUTTON_OPENED: 'toggle.dropdown.confirm-button.opened',
 		};
 	}
 
@@ -44,30 +35,38 @@ class DropDown extends HTMLElement {
 
 	constructor() {
 		super();
+
+		const template = document.getElementById('drop-down-template');
+		const templateContent = template.content;
+
+		const shadowRoot = this.attachShadow({ mode: 'open' });
+		shadowRoot.appendChild(templateContent);
+
 		this.root = $('.js-dropdown');
 		this.items = $('.js-dropdown__items');
 		this.input = $('.js-dropdown__input');
-		this.arrowBtn = $('.js-dropdown__arrow-button');
+		this.arrowButton = $(`.${IconButton.classes.ICON_BUTTON__ARROW}`);
+
 		this.inputEvents = this.inputEvents.bind(this);
 		this.itemsEvents = this.itemsEvents.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 		this.toggleOpened = this.toggleOpened.bind(this);
-		this.arrowBtnEvents = this.arrowBtnEvents.bind(this);
+		this.arrowButtonEvents = this.arrowButtonEvents.bind(this);
 		this.handleKeyPress = this.handleKeyPress.bind(this);
 	}
 
 	connectedCallback() {
 		this.itemsEvents();
 		this.inputEvents();
-		this.arrowBtnEvents();
+		this.arrowButtonEvents();
 	}
 
 	disconnectedCallback() {
 		this.input.off('click.dropdown.input');
 		this.input.off('toggle.dropdown.input.opened');
 		this.items.off('toggle.dropdown.items.opened');
-		this.arrowBtn.off('click.dropdown.arrowBtn');
-		this.arrowBtn.off('toggle.dropdown.arrowBtn.opened');
+		this.arrowButton.off('click.arrow-button.dropdown');
+		this.arrowButton.off('toggle.dropdown.arrow-button.opened');
 	}
 
 	ariaExpandedToggle(elem) {
@@ -90,7 +89,7 @@ class DropDown extends HTMLElement {
 	handleClick() {
 		this.items.triggerHandler('toggle.dropdown.items.opened');
 		this.input.triggerHandler('toggle.dropdown.input.opened');
-		this.arrowBtn.triggerHandler('toggle.dropdown.arrowBtn.opened');
+		this.arrowButton.triggerHandler('toggle.dropdown.arrow-button.opened');
 	}
 
 	handleKeyPress(event) {
@@ -101,7 +100,7 @@ class DropDown extends HTMLElement {
 			case 32:
 				this.items.trigger('toggle.dropdown.items.opened');
 				this.input.trigger('toggle.dropdown.input.opened');
-				this.arrowBtn.trigger('toggle.dropdown.arrowBtn.opened');
+				this.arrowButton.trigger('toggle.dropdown.arrow-button.opened');
 				break;
 			default:
 				break;
@@ -119,14 +118,14 @@ class DropDown extends HTMLElement {
 				elem.toggleClass('js-dropdown__input_opened');
 				this.ariaExpandedToggle(elem);
 				break;
-			case 'js-dropdown__arrow-button':
-				elem.toggleClass('js-dropdown__arrow-button_opened');
+			case IconButton.classes.ICON_BUTTON__ARROW:
+				elem.toggleClass(IconButton.classes.ICON_BUTTON__ARROW_OPENED);
 				break;
 			default:
 				break;
 		}
 	}
-
+	
 	itemsEvents() {
 		this.items.on({
 			'toggle.dropdown.items.opened': this.toggleOpened
@@ -141,10 +140,10 @@ class DropDown extends HTMLElement {
 		});
 	}
 
-	arrowBtnEvents() {
-		this.arrowBtn.on({
-			'click.dropdown.arrowBtn': this.handleClick,
-			'toggle.dropdown.arrowBtn.opened': this.toggleOpened
+	arrowButtonEvents() {
+		this.arrowButton.on({
+			'click.arrow-button.dropdown': this.handleClick,
+			'toggle.dropdown.arrow-button.opened': this.toggleOpened
 		});
 	}
 }
--- src/components/dropdown/dropdown.spec.js
+++ src/components/dropdown/dropdown.spec.js
@@ -1,112 +1,93 @@
-/* eslint-disable object-shorthand */
 const { registerSuite } = intern.getPlugin('interface.object');
 const { expect } = intern.getPlugin('chai');
 const $ = require('jquery');
-const { sleep, domReady } = require('../../utils/js');
 
+const {sleep, domReady} = require('../../utils/js');
 registerSuite('компонент/дропдаун', {
-	beforeEach() {
-		const dropdown = $('.js-dropdown__input');
-		this.dd = dropdown;
-		this.dd.br = this.dd.css('border-radius');
-		this.dd.ae = this.dd.attr('aria-expanded');
-		this.ab = $('.js-dropdown__arrow-button');
-		this.ab.c = this.ab.css('color');
-		this.dl = $('.js-dropdown__items');
-		return this;
-	},
-	tests: {
+    beforeEach() {
+        const dropdown = $('.js-dropdown__input');
+        this.dd = dropdown;
+        this.dd.br = this.dd.css('border-radius');
+        this.dd.ae = this.dd.attr('aria-expanded');
+        this.dl = $('.js-dropdown__items');
+        return this;
+    },
+    tests: {
+        async 'Dropdown имеет ожидаемые классы: загруженный'() {
+            const {dd} = await this.parent;
+            await expect(dd.hasClass('js-dropdown__input_opened')).equal(false);
+        },
 
-		async 'Dropdown имеет ожидаемые классы: загруженный'() {
-			const { dd } = await this.parent;
-			await expect(dd.hasClass('js-dropdown__input_opened')).equal(false);
-		},
+        async 'Aria expanded имеет значение false: загруженный'() {
+            const {dd} = await this.parent;
+            await expect(dd.ae).equal('false');
+        },
 
-		async 'Aria expanded имеет значение false: загруженный'() {
-			const { dd } = await this.parent;
-			await expect(dd.ae).equal('false');
-		},
+        async 'Border radius соответствует ожиданиям: загруженный'() {
+            const {dd} = await this.parent;
+            await expect(dd.br).equal('4px');
+        },
 
-		async 'Border radius соответствует ожиданиям: загруженный'() {
-			const { dd } = await this.parent;
-			await expect(dd.br).equal('4px');
-		},
+        async 'Border сolor имеет ожидаемый цвет: загруженный'() {
+            const bc = $('.js-dropdown__input').css('border-color');
+            await expect(bc).equal('rgba(31, 32, 65, 0.25)');
+        },
 
-		async 'Arrow button имеет ожидаемый цвет: загруженный'() {
-			const { ab } = await this.parent;
-			await expect(ab.c).equal('rgba(31, 32, 65, 0.5)');
-		},
+        async 'Первый клик: Далее dropdown открыт'() {
+            const {dd} = await this.parent;
+            await dd.click();
+            await sleep(1000);
+        },
 
-		async 'Border сolor имеет ожидаемый цвет: загруженный'() {
-			const bc = $('.js-dropdown__input').css('border-color');
-			await expect(bc).equal('rgba(31, 32, 65, 0.25)');
-		},
+        async 'Dropdown имеет ожидаемые классы: открытый'() {
+            const {dd} = await this.parent;
+            await expect(dd.hasClass('js-dropdown__input_opened')).equal(true);
+        },
 
-		async 'Первый клик: Далее dropdown открыт'() {
-			const { dd } = await this.parent;
-			await dd.click();
-			await sleep(1000);
-		},
+        async 'Aria expanded имеет значение true: открытый'() {
+            const {dd} = await this.parent;
+            await expect(dd.ae).equal('true');
+        },
 
-		async 'Dropdown имеет ожидаемые классы: открытый'() {
-			const { dd } = await this.parent;
-			await expect(dd.hasClass('js-dropdown__input_opened')).equal(true);
-		},
+        async 'Border radius соответствует ожиданиям: открытый'() {
+            const {dd} = await this.parent;
+            await expect(dd.br).equal('4px 4px 0px 0px');
+        },
 
-		async 'Aria expanded имеет значение true: открытый'() {
-			const { dd } = await this.parent;
-			await expect(dd.ae).equal('true');
-		},
+        async 'Border сolor имеет ожидаемый цвет: открытый'() {
+            const bc = $('.js-dropdown__input.js-dropdown__input_opened').css('border-color');
+            await expect(bc).equal('rgba(31, 32, 65, 0.5)');
+        },
 
-		async 'Border radius соответствует ожиданиям: открытый'() {
-			const { dd } = await this.parent;
-			await expect(dd.br).equal('4px 4px 0px 0px');
-		},
+        async 'Второй клик: Далее dropdown открыт'() {
+            const {dd} = await this.parent;
+            await dd.click();
+            await sleep(1000);
+        },
 
-		async 'Arrow button имеет ожидаемый цвет: открытый'() {
-			const { ab } = await this.parent;
-			await expect(ab.c).equal('rgba(31, 32, 65, 0.75)');
-		},
 
-		async 'Border сolor имеет ожидаемый цвет: открытый'() {
-			const bc = $('.js-dropdown__input.js-dropdown__input_opened').css('border-color');
-			await expect(bc).equal('rgba(31, 32, 65, 0.5)');
-		},
+        async 'Dropdown имеет ожидаемые классы: закрытый'() {
+            const {dd} = await this.parent;
+            await expect(dd.hasClass('js-dropdown__input_opened')).equal(false);
+        },
 
-		async 'Второй клик: Dropdown далее закрыт'() {
-			const { dd } = await this.parent;
-			await dd.click();
-			await sleep(1000);
-		},
+        async 'Aria expanded имеет значение false: закрытый'() {
+            const {dd} = await this.parent;
+            await expect(dd.ae).equal('false');
+        },
 
-		async 'Dropdown имеет ожидаемые классы: закрытый'() {
-			const { dd } = await this.parent;
-			await expect(dd.hasClass('js-dropdown__input_opened')).equal(false);
-		},
+        async 'Border radius соответствует ожиданиям: закрытый'() {
+            const {dd} = await this.parent;
+            await expect(dd.br).equal('4px');
+        },
 
-		async 'Aria expanded имеет значение false: закрытый'() {
-			const { dd } = await this.parent;
-			await expect(dd.ae).equal('false');
-		},
-
-		async 'Border radius соответствует ожиданиям: закрытый'() {
-			const { dd } = await this.parent;
-			await expect(dd.br).equal('4px');
-		},
-
-		async 'Arrow button имеет ожидаемый цвет: закрытый'() {
-			const { ab } = await this.parent;
-			await expect(ab.c).equal('rgba(31, 32, 65, 0.5)');
-		},
-
-		async 'Border сolor имеет ожидаемый цвет: закрытый'() {
-			const bc = $('.js-dropdown__input').css('border-color');
-			await expect(bc).equal('rgba(31, 32, 65, 0.25)');
-		},
-	}
+        async 'Border сolor имеет ожидаемый цвет: закрытый'() {
+            const bc = $('.js-dropdown__input').css('border-color');
+            await expect(bc).equal('rgba(31, 32, 65, 0.25)');
+        },
+    }
 });
 
-domReady(
-	intern.configure({ reporters: ['console'] }),
-	intern.run()
-);
+domReady(intern.configure({
+    reporters: ['console']
+}), intern.run());
