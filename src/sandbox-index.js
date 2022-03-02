705e647301f6e069c84f88d97e774689927f510c
vo0doo <kirsanov.bvt@gmail.com>
2022-03-02 22:10:03 +0300

    feat: create sandbox-index.js, sandbox-index.pug and sandbox-index.scss - added sandbox index

---
+++ src/sandbox-index.js
@@ -0,0 +1,19 @@
+import './index.scss';
+import './components/dropdown/dropdown.js';
+import './components/counter/counter.js';
+import './components/button/button.js';
+import './components/dropdown/dropdown.spec';
+import './components/button/button.spec';
+import './components/counter/counter.spec';
+
+// console.clear();
+// window.onload = async () => {
+// 	const { default: index } = await import( './index.scss' );
+// 	const { default: textField } = await import( './components/text-field/text-field.js' );
+// 	const { default: dropdown } = await import( './components/dropdown/dropdown.js' );
+// 	const { default: counter } = await import( './components/counter/counter.js' );
+// 	const { default: button } = await import( './components/button/button.js' );
+// 	const { default: dropdownTestSuite } = await import( './components/dropdown/dropdown.spec' );
+// 	const { default: buttonTestSuite } = await import( './components/button/button.spec' );
+// 	const { default: counterTestSuite } = await import( './components/counter/counter.spec' );
+// }
